02a2fc0d591f0e25fe0edee795b89b815103e54d
ghkfla <ryoohelena@gmail.com>
2023-02-20 16:10:17 +0900

    jjj

--- src/nodes/model/model/ModelNode.js
+++ src/nodes/model/model/ModelNode.js
@@ -2,31 +2,45 @@ import { useCallback } from 'react';
 import { Handle, Position } from 'reactflow';
 
 function ModelNode() {
-  const onChange = useCallback((evt) => {
-    console.log(evt.target.value);
-  }, []);
+    const onChange = useCallback((evt) => {
+        console.log(evt.target.value);
+    }, []);
 
-  return (
+    return (
         <div className="model-node">
             <div>
                 <label htmlFor="text" className="model-node_label">MODEL</label>
-                {/* <Menu>
-                    <MenuButton>
-                        Actions
-                    </MenuButton>
-                    <MenuList>
-                        <MenuItem>Download</MenuItem>
-                        <MenuItem>Create a Copy</MenuItem>
-                        <MenuItem>Mark as Draft</MenuItem>
-                        <MenuItem>Delete</MenuItem>
-                        <MenuItem>Attend a Workshop</MenuItem>
-                    </MenuList>
-                </Menu> */}
+                <Select
+                    defaultValue="lucy"
+                    style={{
+                        width: 120,
+                    }}
+                    onChange={handleChange}
+                    options={[
+                        {
+                            value: 'jack',
+                            label: 'Jack',
+                        },
+                        {
+                            value: 'lucy',
+                            label: 'Lucy',
+                        },
+                        {
+                            value: 'Yiminghe',
+                            label: 'yiminghe',
+                        },
+                        {
+                            value: 'disabled',
+                            label: 'Disabled',
+                            disabled: true,
+                        },
+                    ]}
+                />
             </div>
             <Handle type="target" position={Position.Left} />
             <Handle type="source" position={Position.Right} />
         </div>
-  );
+    );
 }
 
 export default ModelNode;
